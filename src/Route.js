63996cad5e40028196d6b32582f76784ad69df0c
Saurabh Gade <saurabhg@Saurabhs-MacBook-Air.local>
2022-05-30 16:30:13 -0400

    optimizes the navigation

--- src/Route.js
+++ src/Route.js
@@ -1,8 +1,23 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 //nested component are recieved with the prop name as 'children' in React
 const Route = ( {path, children} ) => {
-    return (window.location.pathname === path) ? children : null
+
+    const[route, setRoute] = useState(window.location.pathname);
+
+    useEffect(()=>{
+        const onLocationChange = () => {
+            setRoute(window.location.pathname);
+        }
+
+        window.addEventListener('popstate', onLocationChange);
+
+        return ()=>{
+            window.removeEventListener('popstate', onLocationChange)
+        }
+    }, [])
+
+    return (route === path) ? children : null
 }
 
 
